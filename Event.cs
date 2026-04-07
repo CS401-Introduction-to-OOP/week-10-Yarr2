6972c6c52a639e7360ea9a3b584f47d234b4c53f
Yarr2 <yarema.stus@gmail.com>
2026-04-07 18:36:15 +0300

    Added Event class

---
+++ Event.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice10;
+
+public class Event
+{
+    public int MoveNumber { get; }
+    public string Description { get; }
+    public string TypeOfEvent { get; }
+    public string? ChangeOfCharacteristic { get; }
+
+    public Event(int moveNumber, string description, string typeOfEvent, string changeOfCharacteristic)
+    {
+        MoveNumber = moveNumber;
+        Description = description;
+        TypeOfEvent = typeOfEvent;
+        ChangeOfCharacteristic = changeOfCharacteristic;
+    }
+}
