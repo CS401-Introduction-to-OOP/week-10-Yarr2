1d77aa78577de3c1b62d5d6e09debe78097828b3
Yarr2 <yarema.stus@gmail.com>
2026-04-07 18:56:30 +0300

    Added EventLog class with collection of events

---
+++ EventLog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace Practice10;
+
+public class EventLog : IEnumerable<Event>
+{
+    private List<Event> _events = new List<Event>();
+    public void AddEvent(Event inputEvent)
+    {
+        _events.Add(inputEvent);
+    }
+
+    public void RemoveCharacter(Event inputEvent)
+    {
+        _events.Remove(inputEvent);
+    }
+
+
+    public IEnumerator<Event> GetEnumerator()
+    {
+        return _events.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
