3c6ea76f28eebe3565ea78b2da23122bda3fb69b
Yarr2 <yarema.stus@gmail.com>
2026-04-07 18:48:43 +0300

    Added Party class as a collection of characters

---
+++ Party.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Practice10;
+
+public class Party: IEnumerable<Character>
+{
+    private List<Character> _characters = new List<Character>();
+    
+    public void AddCharacter(Character character)
+    {
+        _characters.Add(character);
+    }
+
+    public void RemoveCharacter(Character character)
+    {
+        _characters.Remove(character);
+    }
+
+    public IEnumerator<Character> GetEnumerator()
+    {
+        foreach (var character in _characters)
+        {
+            yield return character;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
